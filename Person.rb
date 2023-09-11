770dfd166fe357aea5d76f5372ccdda5610f6945
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-11 16:09:31 +0300

    update person class

---
+++ Person.rb
@@ -0,0 +1,25 @@
+class Person
+    attr_reader: id, :age, :name
+    attr_writer:name, :age
+
+    def initialize(name = "Unknown", age = 0, parent_permission = true)
+        @id = generate_id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+      end
+
+      def can_use_services?
+        of_age? || @parent_permission
+      end
+
+      private
+
+      def of_age?
+        @age >= 18
+      end
+
+      def generate_id
+        rand(1_000_000..9_999_999)
+      end
+    end
