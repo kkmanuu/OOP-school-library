7c1aaa2d2c542a89af8be36bdf16f12c98dca466
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-13 20:10:29 +0300

    Refactor student class and fix linters

--- student.rb
+++ student.rb
@@ -1,16 +1,18 @@
-require_relative './person'
+require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
+
   def initialize(classroom, age, name, parent_permission)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
-  def classroom= (classsroom)
-     @classroom = classroom
-     classroom.students.include?(self) ?classroom.students : classroom.students.append(self)
-end
+  def classroom=(_classsroom)
+    @classroom = classroom
+    classroom.students.include?(self) ? classroom.students : classroom.students.append(self)
+  end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
