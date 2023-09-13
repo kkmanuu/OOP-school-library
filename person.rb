eaa6c45e8ab59cf68a11ea6bcf56c381b2b0931a
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-13 19:57:16 +0300

    modify the files

--- person.rb
+++ person.rb
@@ -1,16 +1,17 @@
-require_relative 'Nameable'
+require_relative './nameable'
 
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super
+    super()
     @id = Random.rand(1..2000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def correct_name
     @name
@@ -18,6 +19,10 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission == true
+  end
+
+  def add_rental(rental)
+    @rentals.append(rental)
   end
 
   private
