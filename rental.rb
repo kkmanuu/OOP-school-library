725c4ed91d332c86756511ebb8dc14c9bf7b034a
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-14 00:10:48 +0300

    Fix the linters errors

--- rental.rb
+++ rental.rb
@@ -1,15 +1,15 @@
 class Rental
-    attr_accessor :date, :book, :person
+  attr_accessor :date, :book, :person
 
-    def initialize(date, book, person)
-      @date = date
-      @book = book
-      @person = person
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
 
-      # Establish the "belongs-to" association by adding the rental to the book
-      book.rentals << self
+    # Establish the "belongs-to" association by adding the rental to the book
+    book.rentals << self
 
-      # Establish the "belongs-to" association by adding the rental to the person
-      person.rentals << self
-    end
+    # Establish the "belongs-to" association by adding the rental to the person
+    person.rentals << self
   end
+end
