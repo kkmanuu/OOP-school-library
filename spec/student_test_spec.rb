92badbc855c28fc8a6805bda88bb1816ad24b54c
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-21 16:08:30 +0300

    modify the student file and fix the linters

--- spec/student_test_spec.rb
+++ spec/student_test_spec.rb
@@ -2,23 +2,22 @@ require_relative '../classes/classroom'
 require_relative '../classes/student'
 
 describe Student do
-  context 'testing the functionality of class Student'
-  do
-    it'should create an instance of Students' do
-      student = Student.new(45,'samuel',true)
+  context 'Testing the functionality of class Student' do
+    it 'Should create an instance of Student' do
+      student = Student.new(56, 'kkmanuu', true)
       expect(student).to be_a Student
     end
 
     it 'Set the classroom' do
-      student = Student.new(25,'manuu',true)
-      my_classroom = Classroom.new('Project management')
+      student = Student.new(56, 'kkmanuu', true)
+      my_classroom = Classroom.new('Project Management')
       student.classroom = student.classroom = (my_classroom)
       expect(my_classroom).to be_a Classroom
-      expect(student.classroom.label).to eq 'Project management'
+      expect(student.classroom.label).to eq 'Project Management'
     end
 
     it 'Should return an emoji' do
-      student = Student.new(25,'kkmanuu',true)
+      student = Student.new(56, 'kkmanuu', true)
       expect(student.play_hooky).to eq '¯\(ツ)/¯'
     end
   end
