58e6b3f19cb5e90d89f0a298961908645fa44c76
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-09-21 16:09:13 +0300

    modify the teacher file and fix the linters

--- spec/teacher_test_spec.rb
+++ spec/teacher_test_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../classes/teacher'
 
 describe Teacher do
-    context 'Create an instance of the teacher and test methods' do
-        it'create an instance of the teacher' do
-            teacher = Teacher.new('computer', 24, 'Emmanuel')
-            expect(teacher).to be_a Teacher
-        end
-
-        it'Testing can use method' do
-        teacher = Teacher.new('computer',34,'Emmanuel')
-        use_services = teacher.can_use_services?
-        expect(use_services).to eq true
+  context 'Create an instance of the teacher and test methods' do
+    it 'create an instance of the teacher' do
+      teacher = Teacher.new('computer', 24, 'Emmanuel')
+      expect(teacher).to be_a Teacher
     end
-end
+
+    it 'Testing can use method' do
+      teacher = Teacher.new('computer', 34, 'Emmanuel')
+      use_services = teacher.can_use_services?
+      expect(use_services).to eq true
+    end
+  end
 end
